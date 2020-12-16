d597a5a1f7bffb8c481bf0b6368f949b765322e2
steevanvarghese337 <steevanvarghese337@gmail.com>
2020-12-16 16:09:01 +0530

    done

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express =require('express')
-const mongoose =require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const cors =require('cors');
+const cors = require("cors");
 
 mongoose.connect("mongodb://localhost:27017/demo", {
   useNewUrlParser: true,
@@ -12,13 +12,13 @@ mongoose.connect("mongodb://localhost:27017/demo", {
 });
 
 const db = mongoose.connection;
-    db.on("error", (error)=> console.error(error));
-    db.once("open",()=> console.log("connnected to database"));
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connnected to database"));
 
-    const app = express();
+const app = express();
 
-    app.use(cors());
-    app.use(bodyParser.json());
-    const apiRouter = require("./src/routes/api");
-    app.use("/api/v1",apiRouter);
-    app.listen("3000",()=>console.log("server started on"));
+app.use(cors());
+app.use(bodyParser.json());
+const apiRouter = require("./src/routes/api");
+app.use("/api/v1", apiRouter);
+app.listen("3000", () => console.log("server started on 3000"));
--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -1,16 +1,14 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-module.exports = function (req,res,next){
-    const token = req.header('auth-token');
-    if(!token) return res.status(401).send('Access Denied Invalid token !');
-    try{
-        const verified = jwt.verify(token,process.env.TOKEN_SECRET);
-        req.user = verified;
-        next();
-    }
-    catch(err)
-    {
-        console.log(err)
-        res.status(400).send('Invalid Token');
-    }
-}
+module.exports = function (req, res, next) {
+  const token = req.header("auth-token");
+  if (!token) return res.status(401).send("Access Denied Invalid token !");
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Invalid Token");
+  }
+};
